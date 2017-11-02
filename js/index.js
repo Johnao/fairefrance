855108b0863974ad3ee19c80f22bf844969707c8
johnao <john.ao@telecom-sudparis.eu>
2017-11-02 16:17:31 +0100

    + Effets JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	
+	$("#cta p").delay(1000).fadeIn(1000);
+	$("#cta a").delay(1000).animate({
+		marginRight: '0px'
+	}, 1000);
+	$("#pageone ul li").delay(1400).animate({
+		marginLeft: '0px'
+	}, 1000);
 });
